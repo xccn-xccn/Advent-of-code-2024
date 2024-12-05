704ce068a6302ebf165bdb0ea1ccde97ff9a89ad
Jerry Min <116847418+xccn-xccn@users.noreply.github.com>
2024-12-05 10:50:17 +0000

    Update b.py

--- day-4/b.py
+++ day-4/b.py
@@ -1,5 +1,6 @@
 from sys import argv
 from time import perf_counter
+from collections import deque
 
 
 def read_file(filename):
@@ -16,10 +17,31 @@ def get_input_file():
 
 
 def main():
-    text = read_file(get_input_file())
+    grid = read_file(get_input_file()).splitlines()
+    starts = [
+        (y, x)
+        for y, row in enumerate(grid)
+        for x, square in enumerate(row)
+        if square == "A"
+    ]
+    count = 0
+
+    for start in starts:
+        cy, cx = start
+        letters = []
+        for dy, dx in ((1, -1), (-1, 1), (1, 1), (-1, -1)):
+            py, px = cy + dy, cx + dx
+            if py < 0 or py + 1 > len(grid) or px < 0 or px + 1 > len(grid[0]):
+                break
+            letters.append(grid[py][px])
+
+        if sorted(letters[:2]) == sorted(letters[2:]) == ["M", "S"]:
+            count += 1
+    return count
 
 
 if __name__ == "__main__":
     start = perf_counter()
+
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
