6e3181245d0c13843416497569c25b6a8d49cab2
Jerry Min <116847418+xccn-xccn@users.noreply.github.com>
2024-12-05 10:49:57 +0000

    Update a.py

--- day-4/a.py
+++ day-4/a.py
@@ -1,5 +1,6 @@
 from sys import argv
 from time import perf_counter
+from collections import deque
 
 
 def read_file(filename):
@@ -16,27 +17,45 @@ def get_input_file():
 
 
 def main():
-    rules, update = read_file(get_input_file()).split('\n\n')
-    rules = [row.split('|') for row in rules.splitlines()]
-    rules = {s:e for s, e in rules}
+    grid = read_file(get_input_file()).splitlines()
+    starts = [
+        (y, x)
+        for y, row in enumerate(grid)
+        for x, square in enumerate(row)
+        if square == "X"
+    ]
     count = 0
-    for line in update.splitlines():
-        seen = set()
-        valid = True
-        line = line.split(',')
-        for char in line:
-            if rules.get(char, -1) in seen:
-                valid = False
-                break
-            else:
-                seen.add(char)
 
-        if valid:
-            count += line[(len(line)-1)/2]
+    for start in starts:
+        cy, cx = start
+        for dy, dx in (
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+        ):
 
-    print(rules)
+            for i in range(1, 4):
+
+                py, px = cy + dy * i, cx + dx * i
+                if py < 0 or py + 1 > len(grid) or px < 0 or px + 1 > len(grid[0]):
+                    continue
+
+                if grid[py][px] == "XMAS"[i]:
+                    if i == 3:
+                        count += 1
+                else:
+                    break
+
+    return count
+
 
 if __name__ == "__main__":
     start = perf_counter()
+
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
