bf44403dd0b82f62ca59b0d8d460e5d41ef402a0
Jerry Min <jerryjmin7@gmail.com>
2024-12-11 18:48:52 +0000

    day-11

--- day-11/a.py
+++ day-11/a.py
@@ -1,6 +1,7 @@
 from sys import argv
 from time import perf_counter
 
+
 def read_file(filename):
     with open(filename) as file:
         text = file.read()
@@ -16,15 +17,15 @@ def get_input_file():
 
 def main():
     stones = [int(x) for x in read_file(get_input_file()).split()]
-    for r in range(25):
+    for _ in range(25):
         new = []
         for s in stones:
             str_s = str(s)
             if s == 0:
                 new.append(1)
             elif len(str_s) % 2 == 0:
-                new.append(int(str_s[:len(str_s)//2]))
-                new.append(int(str_s[len(str_s)//2:]))
+                new.append(int(str_s[: len(str_s) // 2]))
+                new.append(int(str_s[len(str_s) // 2 :]))
             else:
                 new.append(s * 2024)
 
@@ -37,4 +38,4 @@ if __name__ == "__main__":
     start = perf_counter()
 
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
--- day-11/b.py
+++ day-11/b.py
@@ -1,5 +1,7 @@
 from sys import argv
 from time import perf_counter
+from functools import cache
+
 
 def read_file(filename):
     with open(filename) as file:
@@ -15,11 +17,33 @@ def get_input_file():
 
 
 def main():
-    text = read_file(get_input_file())
+    stones = [int(x) for x in read_file(get_input_file()).split()]
+    count = 0
+    for s in stones:
+        count += get_stones(s, 75)
+
+    return count
+
+
+@cache
+def get_stones(stone, rem):
+    if rem == 0:
+        return 1
+
+    str_s = str(stone)
+    if stone == 0:
+        return get_stones(1, rem - 1)
+    elif len(str_s) % 2 == 0:
+        return get_stones(int(str_s[: len(str_s) // 2]), rem - 1) + get_stones(
+            int(str_s[len(str_s) // 2 :]), rem - 1
+        )
+
+    else:
+        return get_stones(stone * 2024, rem - 1)
 
 
 if __name__ == "__main__":
     start = perf_counter()
 
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
