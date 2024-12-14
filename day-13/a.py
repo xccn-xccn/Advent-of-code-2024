07e5bf62865b54434666f65aeed174672223101e
Jerry Min <jerryjmin7@gmail.com>
2024-12-14 01:41:16 +0000

    day-13

--- day-13/a.py
+++ day-13/a.py
@@ -1,5 +1,7 @@
 from sys import argv
 from time import perf_counter
+from collections import defaultdict
+import re
 
 
 def read_file(filename):
@@ -16,10 +18,61 @@ def get_input_file():
 
 
 def main():
-    text = read_file(get_input_file())
+    machines = [
+        list(map(int, list(re.findall("\d+", x))))
+        for x in read_file(get_input_file()).split("\n\n")
+    ]
+    possible = []
+    for mach in machines:
+        p = single(mach)
+        if p != float("inf"):
+            possible.append(p)
+
+    return sum(possible)
+
+
+def single(machine):
+    bag = [(0, (0, 0))]  # tokens score
+    best = float("inf")
+    seen = defaultdict(lambda: float("inf"))
+    while bag:
+        ct, cp = bag.pop()
+        cx, cy = cp
+        for dt, d in zip((3, 1), (machine[:2], machine[2:4])):
+            dx, dy = d
+            pt, px, py = ct + dt, cx + dx, cy + dy
+            if px > machine[4] or py > machine[5] or seen[(px, py)] <= pt:
+                continue
+            if px == machine[4] and py == machine[5]:
+                if pt < best:
+                    best = pt
+                else:
+                    return pt
+            else:
+                bag.insert(binary_search(bag, pt), (pt, (px, py)))
+                seen[(px, py)] = pt
+
+    return best
+
+
+def binary_search(bag, aim):
+    l, h = 0, len(bag) - 1
+    m = 0
+    while l <= h:
+        m = (l + h) // 2
+
+        p = bag[m][0]
+        if p == aim:
+            return m
+        elif p < aim:
+            h = m - 1
+        else:
+            l = m + 1
+
+    return m
 
 
 if __name__ == "__main__":
     start = perf_counter()
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
--- day-13/b.py
+++ day-13/b.py
@@ -1,5 +1,7 @@
 from sys import argv
 from time import perf_counter
+import numpy as np
+import re
 
 
 def read_file(filename):
@@ -16,10 +18,53 @@ def get_input_file():
 
 
 def main():
-    text = read_file(get_input_file())
+    np.set_printoptions(suppress=True)
+    machines = [
+        list(map(int, list(re.findall("\d+", x))))
+        for x in read_file(get_input_file()).split("\n\n")
+    ]
+    c_machines = machines
+    machines = []
+    for m in c_machines:
+        m[4] += 10000000000000
+        m[5] += 10000000000000
+        machines.append(m)
+    count = 0
+    for mach in machines:
+        p = single(mach)
+        if p is not False and all(n % 1 <= 10**-2 or n % 1 >= (1 - 10**-2) for n in p):
+            count += p[0] * 3 + p[1]
+
+    return count
+
+
+def single(machine):
+    b1, b2 = machine[:4:2], machine[1:4:2]
+    m1 = [b1, b2]
+    m2 = machine[4:]
+    if np.linalg.det(m1) == 0:
+        return False
+    return np.matmul(np.linalg.inv(m1), m2)
+
+
+def binary_search(bag, aim):
+    l, h = 0, len(bag) - 1
+    m = 0
+    while l <= h:
+        m = (l + h) // 2
+
+        p = bag[m][0]
+        if p == aim:
+            return m
+        elif p < aim:
+            h = m - 1
+        else:
+            l = m + 1
+
+    return m
 
 
 if __name__ == "__main__":
     start = perf_counter()
     print(main())
-    print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+    print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
